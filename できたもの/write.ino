5216bf3cc7c9768f3f3963a10f3ed6442e98a8f5
ognknt <k661301@kansai-u.ac.jp>
2022-09-04 00:31:28 +0900

    GUIの作成

---
+++ できたもの/write.ino
@@ -0,0 +1,22 @@
+void setup(){
+	pinMode(PB2,OUTPUT);
+	pinMode(PB4,OUTPUT);
+	pinMode(PB1,OUTPUT);
+}
+void loop(){
+	analogWrite(PB2,221);
+	analogWrite(PB4,91);
+	analogWrite(PB1,255);
+	delay(500);
+
+	analogWrite(PB2,190);
+	analogWrite(PB4,255);
+	analogWrite(PB1,79);
+	delay(500);
+
+	analogWrite(PB2,229);
+	analogWrite(PB4,56);
+	analogWrite(PB1,172);
+	delay(754);
+
+}
