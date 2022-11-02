3edfb33988539af2cef8a9624346663e8111ec8d
teacha1025 <teacha1025@gmail.com>
2022-11-02 10:10:48 +0900

    修正版

--- できたもの/gui/attiny13a_control/attiny13a_control.ino
+++ できたもの/gui/attiny13a_control/attiny13a_control.ino
@@ -4,6 +4,25 @@ constexpr int RED = 11;
 constexpr int GREEN = 10;
 constexpr int BLUE = 9;
 
+void set(unsigned long ms,unsigned int pb1,unsigned int pb4,unsigned int pb3){
+  analogWrite(GREEN, pb1);
+  analogWrite(BLUE, pb4);
+  if (pb3 == 0U || pb3 == 255U) {
+    digitalWrite(RED, pb3 == 255U ?  HIGH : LOW);
+    delay(ms);
+    return;
+  }
+
+  for (long us = ms * 1000L / 255L; us > 0L; us--) {
+    digitalWrite(RED, HIGH);
+    delayMicroseconds(pb3);
+    
+    digitalWrite(RED, LOW);
+    delayMicroseconds(255u - pb3);
+  }
+  
+}
+
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(CHECK, OUTPUT);
@@ -18,12 +37,15 @@ void loop() {
   //digitalWrite(CHECK, HIGH);
   Serial.write(0);//準備okのサイン
 
-  analogWrite(RED, r);
-  analogWrite(GREEN, g);
-  analogWrite(BLUE, b);
+  //analogWrite(RED, r);
+  //analogWrite(GREEN, g);
+  //analogWrite(BLUE, b);
 
-
-  delay(10);
+  set(10, g,b,r);
+  //set(1000, 255,255,255);
+  //set(1000, 0,0,127);
+  //set(1000, 5,5,254);
+  //set(1000, 0,0,255U);
 }
 /*/////////////////////////////////////////*/
 void serialEvent() { //シリアルに4バイト以上の値が送られてきたときに行う処理
